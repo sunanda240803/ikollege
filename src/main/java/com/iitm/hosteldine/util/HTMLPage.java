f7a23a2f5a12c777b488cc967bae8a423c9447bd
unknown <finapgny2@fintechasiapacific.com>
2026-09-02 10:17:29 +0530

    Add Hostel Capacity Summary module with exact capacity calculations and consolidated overview

--- src/main/java/com/iitm/hosteldine/util/HTMLPage.java
+++ src/main/java/com/iitm/hosteldine/util/HTMLPage.java
@@ -19,6 +19,10 @@ public class HTMLPage {
 	public static final String VIEW_INVENTORY = "student/view-inventory";
 
 	// Hostel Master
+	public static final String HOSTEL_CAPACITY = "hostel/hostel-capacity";
+	public static final String HOSTEL_CAPACITY_MODULE = "hostel/hostel-capacity";
+	public static final String HOSTEL_UTILIZATION_REPORT = "hostel/hostel-utilization-report";
+	public static final String YEARWISE_HOSTEL_CAPACITY_REPORT = "hostel/yearwise-hostel-capacity-report";
 	public static final String HOSTEL_MASTER = "hostel/hostel-master";
 	public static final String ADD_EDIT_HOSTEL_MASTER = "hostel/hostel-master-modal";
 	public static final String FLOOR_MASTER = "hostel/floor-master";
